b75bca482b6d962041e4247ccc86211b36d04f4f
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-18 21:02:16 +0000

    Ejercicio 9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        num = number % -10
+        num = abs(num)
+        print('{:d}'.format(num), end='')
+        return(num)
+    else:
+        num = number % 10
+        print('{:d}'.format(num), end='')
+        return(num)
