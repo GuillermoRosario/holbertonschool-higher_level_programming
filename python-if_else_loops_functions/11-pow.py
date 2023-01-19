149c75dbad45d154f076999d20f40b31b7eb3c22
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-19 17:19:17 +0000

    Ejercicio 11

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -2,6 +2,12 @@
 def pow(a, b):
     base = a
     exp = b 
+    if a < 0
+    for i in range(0, exp -1)
+        base = base * base
+    result = 1 / base
+    return (result)
+    if a > 0
     for i in range(0, exp - 1):
         base = base * base
     result = base
