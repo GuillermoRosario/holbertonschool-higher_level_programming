0eff303895def6960c41438af45b3e4dbdb5c0d0
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-20 21:18:14 +0000

    ejercicio 2

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 a = 10 
 b = 5
 
-print(f"{a} + {b} = {add}")
-print(f"{a} - {b} = {sub}")
-print(f"{a} * {b} = {mult}")
-print(f"{a} / {b} = {div}")
+print(f"{a} + {b} = {add}").format(a, b add(a, b)))
+print(f"{a} - {b} = {sub}").format(a, b sub(a, b)))
+print(f"{a} * {b} = {mult}").format(a, b sub(a, b)))
+print(f"{a} / {b} = {div}").format(a, b sub(a, b)))
