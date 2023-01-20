df98f9b678ca73519e99ad44e372742d693f14df
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-20 17:14:53 +0000

    ejercicio 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def add(a, b):
-    a = 1
-    b = 2
-    i = sum
-    add = "a + b = i"
-    print("{}".format(add)) 
+    if __name__ == "__main__":
+    from add_0 import add
+        a = 1
+        b = 2
+    print("{} + {} = {}".format(a,b,add(a,b)) 
---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add(a, b):
+    a = 1
+    b = 2
+    i = sum
+    add = "a + b = i"
+    print("{}".format(add))
