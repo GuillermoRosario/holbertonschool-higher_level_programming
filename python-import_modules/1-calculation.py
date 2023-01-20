bcd84125219cb2fefc9e3a03b03131baa66607cc
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-20 19:40:54 +0000

    ejercicio 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-def add(a, b):
-    a = 1
-    b = 2
-    i = sum
-    add = "a + b = i"
-    print("{}".format(add))
+if __name__ == "__main__":
+    from calculator_1.py import add, mult, sub, div
+a = 10 
+b = 5
+
+print(f"{a} + {b} = {add}")
+print(f"{a} - {b} = {sub}")
+print(f"{a} * {b} = {mult}")
+print(f"{a} / {b} = {div}")
