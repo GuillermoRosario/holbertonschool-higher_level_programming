ae18c321fd77267fb26a7faced8f76b51284d7d3
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-20 21:21:20 +0000

    ejercicio 2

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1.py import add, mult, sub, div
+    from calculator_1 import add, mul, sub, div
 a = 10 
 b = 5
 
-print(f"{a} + {b} = {add}").format(a, b add(a, b)))
-print(f"{a} - {b} = {sub}").format(a, b sub(a, b)))
-print(f"{a} * {b} = {mult}").format(a, b sub(a, b)))
-print(f"{a} / {b} = {div}").format(a, b sub(a, b)))
+print("{} + {} = {}").format(a, b add(a, b)))
+print("{} - {} = {}").format(a, b sub(a, b)))
+print("{} * {} = {}").format(a, b mul(a, b)))
+print("{} / {} = {}").format(a, b div(a, b)))
