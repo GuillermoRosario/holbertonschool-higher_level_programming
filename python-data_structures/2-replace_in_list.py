cd4fbb99d9e45a79752f49e53282a7a3be9400ba
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-23 19:44:50 +0000

    condition correction

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-
-
+    
     List = len(my_list)
-
     if idx < 0 or idx > List:
         return my_list
     if idx > 0 or idx < List:
