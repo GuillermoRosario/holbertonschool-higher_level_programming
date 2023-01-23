dc70c8a340d591e80e44abd7dd207d51c79e4af8
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-23 16:58:16 +0000

    Print List 1

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -2,4 +2,5 @@
 
 def print_list_integer(my_list=[]):
     my_list = [1, 2, 3 , 4, 5]
-    print("{} + \n".format(my_list))
+    result = f'{my_list[0]} {my_list[1]} {my_list[2]} {my_list[3]} {my_list[4]}'
+    print(result)
