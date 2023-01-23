0dbbe64e7cc0122a77a744b33b961697b7a75789
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-23 19:35:00 +0000

    Correction P8

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@
 def replace_in_list(my_list, idx, element):
  
     List = len(my_list)
-    if idx < 0 or idx > List - 1:
+
+    if idx < 0 or idx > List:
+        my_list[idx] = element
         return my_list
-    my_list[idx] = element
-    print(my_list)
