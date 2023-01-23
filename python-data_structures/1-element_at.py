19ef171ccf71a837c32aaf58ca8e04326433ff2c
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-23 18:21:55 +0000

    Elements at index

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,8 +2,6 @@
 def element_at(my_list, idx):
     if idx < len(my_list):
         return None
-    elif idx == 0:
-        return len(my_list)
     elif idx > len(my_list):
         return None
     print("Element at index {:d} is {}".format(idx, my_list))
