2414d2d910bde2c51fe2ef736a6a9a9c877a9c1c
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-23 19:31:10 +0000

    Correction P8

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -5,7 +5,5 @@ def replace_in_list(my_list, idx, element):
     List = len(my_list)
     if idx < 0 or idx > List - 1:
         return my_list
-
     my_list[idx] = element
-    print(new_list)
     print(my_list)
