5c1042206d012ff7041e180f462a42f2937ea4e1
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-23 19:27:01 +0000

    Correction P8

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-element = 9
 
 def replace_in_list(my_list, idx, element):
  
     List = len(my_list)
-    if idx < 0 or idx > List:
+    if idx < 0 or idx > List - 1:
         return my_list
-    my_list[idx] = element
 
+    my_list[idx] = element
     print(my_list)
