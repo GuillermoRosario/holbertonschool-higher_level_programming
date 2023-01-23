054b558abafee4cb5c8ab70d58a983db57827e5d
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-23 17:17:32 +0000

    Print List 1

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
+my_list = [1, 2, 3, 4, 5]
 def print_list_integer(my_list=[]):
-    my_list = [1, 2, 3 , 4, 5]
-    result = f'{my_list[0]} {my_list[1]} {my_list[2]} {my_list[3]} {my_list[4]}'
-    print(result)
+    for i in my_list:
+        print("{:d}".format(i))
