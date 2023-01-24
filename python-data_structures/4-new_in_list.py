c0b5a30773e07b589fa3e9493eb3389a596f1643
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-24 00:08:26 +0000

    List Index Replace

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,10 +2,11 @@
 
 def new_in_list(my_list, idx, element):
 
+
     new_list = list(my_list)
     length = len(my_list) - 1
 
-    if idx > length or idx < 0: 
+    if idx > length or idx < 0:
         return new_list
 
     new_list[idx] = element
