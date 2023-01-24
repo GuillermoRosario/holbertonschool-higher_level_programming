4f2968b031cb0345d8efeae53c1e7715dbe03761
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-24 00:07:04 +0000

    Reversed List

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3i
+
+def new_in_list(my_list, idx, element):
+
+    new_list = list(my_list)
+    length = len(my_list) - 1
+
+    if idx > length or idx < 0: 
+        return new_list
+
+    new_list[idx] = element
+
+    return new_list
