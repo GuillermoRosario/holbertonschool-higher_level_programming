67e1dbc9838793b5cf1729cab912a8323ac579b7
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-25 02:30:30 +0000

    Tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-
-    new_tuple = add_tuple(tuple_a, tuple_b)
-    res = []
-    for i in range(0,len(tuple_a)):
-            res.append(tuple_a[i]+tuple_b[i])
-            res = tuple(res)
-        print(str(res))
+    if len(tuple_a) < 2:
+        tuple_a += (0, 0)
+    if len(tuple_b) < 2:
+        tuple_b += (0, 0)
+    a = tuple_a[0] + tuple_b[0]
+    b = tuple_a[1] + tuple_b[1]
+    return(a,b)
