2a1e5bb94972d1ba4922183a6048c2adb69cb924
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-27 01:58:46 +0000

    Unique set add

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
 
-    def get_unique(my_list=[]):
-
         unique_list = set(my_list)
-        for i in unique_list:
-            unique_list.append(my_list)
-            return unique_list
-    print(get_unique)
-
-
-
+        unique_add = 0
+        for number in unique_list:
+            unique_add += number
+            return(unique_add)
