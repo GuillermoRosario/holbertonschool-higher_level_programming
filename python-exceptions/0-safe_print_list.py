af8b05cfda103ed583d03707650e01fc12c80ba6
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-27 14:58:43 +0000

    Function that prints

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+
+    elements = my_list=[]
+    result = x[0]
+    try:
+        print("{:d}".format(result))
+    except:
+        print("An exception occurred")
