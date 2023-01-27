b1c2d288b99c8c7e69ee190fcaecb84503ff2f66
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-27 01:43:33 +0000

    Unique set add

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+
+    def get_unique(my_list=[]):
+
+        unique_list = set(my_list)
+        for i in unique_list:
+            unique_list.append(my_list)
+            return unique_list
+    print(get_unique)
+
+
+
