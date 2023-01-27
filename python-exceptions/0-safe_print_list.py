e9cb264ff1c7ce4d4dcef4212e7e0a476997e498
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-27 15:21:00 +0000

    new list replace

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-
-    elements = my_list=[]
-    result = x[0]
     try:
-        print("{:d}".format(result))
-    except:
-        print("An exception occurred")
+        for num in range(x):
+            print(my_list[num], end="")
+        print()
+        return x
+    except: 
+        print()
+        return num 
