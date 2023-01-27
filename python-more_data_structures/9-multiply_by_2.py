db78adb1a2a5e18ee2b1b5fd3b9a08d5855769c8
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-27 03:08:04 +0000

    delte dictionary

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+
+
 def multiply_by_2(a_dictionary):
 
-    new_dictionary = {}
+    _dictionary = {}
 
     for k, v in a_dictionary.items():
 
-        new_dictionary[k] = v * 2
+        _dictionary[k] = v * 2
 
-    return new_dictionary
+    return _dictionary
