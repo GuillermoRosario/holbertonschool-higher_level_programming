28f33caa1e71496014faa91dc5a20adb39505e1a
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-30 18:21:22 +0000

    Square Class

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,8 +6,8 @@ class Square:
     """Write an empty class square that defines a square"""
     def __init__(self, size=0):
         if type(size) != int:
-            raise TypeError ("size must be an integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError ("size must be >= 0")
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
