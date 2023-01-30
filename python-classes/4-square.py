aac1c8031e25a0a6e364428ffabafd176e8747d0
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-30 21:03:17 +0000

    Getter & Setters

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+
 """Creation of an empty class defining a Square"""
 
 
 class Square:
-
     """Write an empty class square that defines a square"""
     def __init__(self, size=0):
         """Function to retrieve"""
@@ -29,4 +29,3 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-
