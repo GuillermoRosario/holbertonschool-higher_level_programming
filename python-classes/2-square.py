c82a9468211833b68afde034658cb712d0125b2f
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-30 18:13:54 +0000

    Square Class

---
+++ python-classes/2-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Creation of an empty class defining a Square"""
+
+
+class Square:
+    """Write an empty class square that defines a square"""
+    def __init__(self, size=0):
+        if size != 0:
+            raise TypeError ("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
