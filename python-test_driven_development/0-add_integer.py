b3b76a8070b82abb0d54194b894e356cfcfb8961
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-01 17:37:09 +0000

    adding integers

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    if a or b != int or float: 
+        raise TypeError ("a must be an integer") 
+        raise TypeError ("b must be an integer")
+    elif a and b == int or float:
+        return a + b
