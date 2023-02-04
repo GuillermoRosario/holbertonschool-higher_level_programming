af9e2b7927b4ebb4c9791423d44dd09085eb393a
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-04 13:01:00 -0400

    Fixing PPe8

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,15 +6,15 @@ class Rectangle:
     """Defines a Rectangle"""
     def __init__(self, width=0, height=0):
         if type(width) != int:
-            raise TypeError ("width must be an integer")
+            raise TypeError ('width must be an integer')
         elif width < 0:
-            raise ValueError ("width must be >= 0")
+            raise ValueError ('width must be >= 0')
         else:
             self.__width = width
         if type(height) != int: 
-            raise TypeError ("height must be an integer")
+            raise TypeError ('height must be an integer')
         elif height < 0:
-            raise ValueError ("height must be >= 0")
+            raise ValueError ('height must be >= 0')
         else:
             self.__height = height
 
@@ -23,23 +23,25 @@ class Rectangle:
         @property
         def width(self):
             return self.__width
+        
         @width.setter
         def width(self, value):
-            if type(value) is not int: 
+            if type(value) != int: 
                 raise TypeError('width must be an integer')
             elif value < 0: 
                 raise ValueError('width must be >= 0')
             else: 
                 self.__width = value
+
         @property
         def height(self):
             return self.__height
         
         @height.setter
         def height(self, value):
-            if type(value) is not int:
+            if type(value) != int:
                 raise TypeError('height must be an integer')
             elif value < 0:
                 raise ValueError('height must be >= 0')
             else:
-                self.__height = value
+                self.__height = value 
