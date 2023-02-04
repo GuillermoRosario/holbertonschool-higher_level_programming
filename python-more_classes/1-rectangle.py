ff384884c2b01ec9c26ba71997ae95c6b5ba2517
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-04 12:43:16 -0400

    Getters and Setters

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""This module is a Rectangle Class"""
+
+
+class Rectangle:
+    """Defines a Rectangle"""
+    def __init__(self, width=0, height=0):
+        if type(width) != int:
+            raise TypeError ("width must be an integer")
+        elif width < 0:
+            raise ValueError ("width must be >=0")
+        else:
+            self.__width = width
+        if type(height) != int: 
+            raise TypeError ("height must be an integer")
+        elif type(height) < 0:
+            raise ValueError ("height must be >=0")
+        else:
+            self.__height = height
+
+        """ Creating Getter & Setters for each argument"""
+
+        @property
+        def width(self):
+            return self.__width
+        @width.setter
+        def width(self, value):
+            if type(value) is not int: 
+                raise TypeError('width must be an integer')
+            elif value < 0: 
+                raise ValueError('width must be >= 0')
+            else: 
+                self.__width = value
+        @property
+        def height(self):
+            return self.__height
+        
+        @height.setter
+        def height(self, value):
+            if type(value) is not int:
+                raise TypeError('height must be an integer')
+            elif value < 0:
+                raise ValueError('height must be >= 0')
+            else:
+                self.__height = value
+       
+        
