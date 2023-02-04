3348b2c638555adbed74ac30e866431410d2cda7
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-04 12:53:52 -0400

    Fixing Indent

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,13 +8,13 @@ class Rectangle:
         if type(width) != int:
             raise TypeError ("width must be an integer")
         elif width < 0:
-            raise ValueError ("width must be >=0")
+            raise ValueError ("width must be >= 0")
         else:
             self.__width = width
         if type(height) != int: 
             raise TypeError ("height must be an integer")
         elif height < 0:
-            raise ValueError ("height must be >=0")
+            raise ValueError ("height must be >= 0")
         else:
             self.__height = height
 
@@ -43,5 +43,3 @@ class Rectangle:
                 raise ValueError('height must be >= 0')
             else:
                 self.__height = value
-       
-        
