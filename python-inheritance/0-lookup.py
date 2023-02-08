758c5892ac919dedb139aa5203664da9aba23433
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-08 11:01:06 -0400

    Documentation

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def lookup(obj):
+
     """Call function dir that returns the list of 
-       available attributes and methods of obj"""
+        available attributes and methods of obj"""
+
     return dir(lookup)
