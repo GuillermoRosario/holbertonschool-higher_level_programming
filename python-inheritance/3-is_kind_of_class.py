71879a8b41cc3c5ae65185ae799008384c80076a
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-08 15:15:14 -0400

    Lo Nuevo

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+
+
+"""This is the Declaration of a Function"""
+
+
 def is_kind_of_class(obj, a_class):
+
+    """Creating the Class Function"""
+
     return type(obj) is a_class
