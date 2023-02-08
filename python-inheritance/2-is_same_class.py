7f5b097c9191779d9ba31aa04fb872503392da3b
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-08 13:26:21 -0400

    Ejercicio 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Defines main Function"""
+
+def is_same_class(obj, a_class):
+
+    """Variable declaration and Condition"""
+
+    a_class = 1
+
+    if a_class == is_same_class:
+        return True
+    elif a_class != is_same_class:
+        return False
