30ec737a860c7a073f7d187ca38b29822845ffa7
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-09 14:31:29 -0400

    Space Correction

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -2,9 +2,9 @@
 """
 Module that returns true or false
 """
-def inherits_from(obj, a_class):
 
-    
+
+def inherits_from(obj, a_class):
     """
     function that returns True if the object is an instance of a class
     that inherited(directly or indirectly) from the specified class if not
