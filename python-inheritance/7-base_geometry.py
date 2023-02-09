8a8202cb0a8c0019079f800325e7d80bafd15806
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-09 16:45:37 -0400

    PEP8

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,7 +12,7 @@ class BaseGeometry:
         """
         Method that validates value
         """
-        if type(value) is not int: 
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
