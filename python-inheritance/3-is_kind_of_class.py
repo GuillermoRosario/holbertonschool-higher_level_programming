b768b056977d32d6c486c92a76e896b2492dd395
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-09 13:07:51 -0400

    is intance correction

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -6,8 +6,9 @@ if object passed is instance the class is passed
 
 
 def is_kind_of_class(obj, a_class):
-    """Returns true in Obj is an instance"""
-
+    """
+    Returns true in Obj is an instance
+    """
     if isintance(obj, a_class)
         return True
     else:
