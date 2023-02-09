58c12bd4a9b083a05dc2fb754dcbf5811248da68
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-09 13:06:07 -0400

    is intance correction

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python3
-
-
-"""This is the Declaration of a Function"""
+#!/usr/bin/python
+"""
+This is the Declaration of a Function that
+if object passed is instance the class is passed
+"""
 
 
 def is_kind_of_class(obj, a_class):
-
-    """Creating the Class Function"""
+    """Returns true in Obj is an instance"""
 
     if isintance(obj, a_class)
         return True
