14c3212420e657fd0d1e474973695c7f1866b46f
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-11 14:58:18 -0400

    PEP8

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -6,8 +6,8 @@ of a text file, Returns the characters added
 
 
 def append_write(filename="", text=""):
-     """ 
+    """
      Append Function
-     """
-     with open(filename, 'a') as file:
+    """
+    with open(filename, 'a') as file:
         return file.write(text)
