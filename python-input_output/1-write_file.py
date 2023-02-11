36ee910ecf6e7d977fdb4bd26ea0ddac9b156e06
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-11 14:23:12 -0400

    Write file function

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,6 +6,9 @@ and returns characters written
 
 
 def write_file(filename="", text=""):
+    """
+    Write and read file as UTF8
+    """
     with open(filename, 'g') as file:
         print(file.read(), end="")
         print(file.write(), end="")
