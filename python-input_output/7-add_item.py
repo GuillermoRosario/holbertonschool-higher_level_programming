14d0a5d7fa27bfee782886814642bfea2fe0c484
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-11 23:12:16 -0400

    PEP8

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-"""
-Script: Adds all arguments to a Python list,
-and then saves them to a file
-"""
+"""comment Module"""
 import sys
+import json
 
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+def save_to_json_file(my_obj, filename):
+    """comment fucntion"""
+    with open(filename, 'w') as f:
+        json.dump(my_obj, f)
 
-args = sys.argv
+
+def load_from_json_file(filename):
+    """comment function"""
+    with open(filename, 'r') as f:
+        return json.load(f)
+
 filename = "add_item.json"
-my_list = []
-
 try:
-    my_list = load_from_json_file(filename)
+    items = load_from_json_file(filename)
 except:
-    pass
-for i in range(1, len(args)):
-    my_list.append(args[i])
-save_to_json_file(my_list, filename)
+    items = []
+
+for arg in sys.argv[1:]:
+    items.append(arg)
+
+save_to_json_file(items, filename)
