ab8f322d09f4ba4373f5247d1e62301f529a89d4
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-13 13:14:06 -0400

    Ejercicio 1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+Create a class named Base
+"""
+class Base:
+    """
+    Base comment Module
+    """
+    _nb_objects = 0
+
+def __init__(self, id=None):
+    if id is not None:
+        self.id = id
+    else:
+        Base.__nb_objects += 1
+        self.id = Base.__nb_objects
