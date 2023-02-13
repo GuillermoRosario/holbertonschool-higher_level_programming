ed096905b69df30fc1919628ee3d7ee844c1c967
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-13 16:35:24 -0400

    PEP8

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -17,11 +17,11 @@ class Rectangle(Base):
         """
         if type(width) is not int:
             raise TypeError('width must be an integer')
-        elif width <= 0: 
+        elif width <= 0:
             raise ValueError('width must be > 0')
-        else: 
+        else:
             self.__width = width
-        if type(height) is not int: 
+        if type(height) is not int:
             raise TypeError('height must be an integer')
         elif height <= 0:
             raise ValueError('height must be > 0')
@@ -29,13 +29,13 @@ class Rectangle(Base):
             self.__height = height
         if type(x) is not int:
             raise TypeError('x must be an integer')
-        elif x < 0: 
+        elif x < 0:
             raise ValueError('x must be >= 0')
         else:
             self.__x = x
         if type(y) is not int:
             raise TypeError('y must be an integer')
-        elif y < 0: 
+        elif y < 0:
             raise ValueError('y must be >= 0')
         else:
             self.__y = y
@@ -48,9 +48,9 @@ class Rectangle(Base):
     def width(self, width):
         if type(width) is not int:
             raise TypeError('width must be an integer')
-        elif width <= 0: 
+        elif width <= 0:
             raise ValueError('width must be > 0')
-        else: 
+        else:
             self.__width = width
 
     @property
@@ -59,7 +59,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, height):
-        if type(height) is not int: 
+        if type(height) is not int:
             raise TypeError('height must be an integer')
         elif height <= 0:
             raise ValueError('height must be > 0')
@@ -74,11 +74,10 @@ class Rectangle(Base):
     def x(self, x):
         if type(x) is not int:
             raise TypeError('x must be an integer')
-        elif x < 0: 
+        elif x < 0:
             raise ValueError('x must be >= 0')
         else:
             self.__x = x
-    
 
     @property
     def y(self):
