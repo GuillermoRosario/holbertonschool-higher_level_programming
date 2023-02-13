e4b7eb4d300c86f4868fa4dd61f93cb30af3c04c
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-13 13:16:48 -0400

    PEP8

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,15 +2,17 @@
 """
 Create a class named Base
 """
+
+
 class Base:
     """
     Base comment Module
     """
     _nb_objects = 0
 
-def __init__(self, id=None):
-    if id is not None:
-        self.id = id
-    else:
-        Base.__nb_objects += 1
-        self.id = Base.__nb_objects
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
