9d58ed0ccc482179ecad81c4f1d3a29aa9406adf
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-15 12:01:25 -0400

    PEP8

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -111,12 +111,12 @@ class Rectangle(Base):
         that it returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
         return (f'[Rectangle] ({self.id}) {self.__x}/{self.__y} - '
                 f'{self.__width}/{self.__height}')
-    
+
     def update(self, *args):
         """
         assigns an argument to each attribute
         """
-        argument = 0 
+        argument = 0
         if args is not None:
             for attribute in args:
                 if argument == 0:
