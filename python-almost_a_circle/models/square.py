61774b878ff4a4b8c4fbefc7e32e098f95c02326
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-15 13:05:39 -0400

    Square Class Creation and Str

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,17 +9,17 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-   """
-   Inherited Class receiving size, x, y, and id
-   """
-   def __init__(self, size, x=0, y=0, id=None):
     """
-    Validates super class attributes
-    and assigns them
+    Inherited Class receiving size, x, y, and id
     """
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Validates super class attributes
+        and assigns them
+        """
     super().__init__(size, size, x, y, id)
 
-   def __str__(self):
+    def __str__(self):
         """
         String Rep of the Square Object
         Returns [Square] (<id>) <x>/<y> - <size>
