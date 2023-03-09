a8c6bed66314b6d4b2aca497bf2cd95174d42baf
GuillermoRosario <guillermorosario628@gmail.com>
2023-03-09 14:42:34 -0400

    4 SQL

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
 -- Create Table in MYSQL Database
-CREATE TABLE first_table (
-id INT
-name VARCHAR(256));
+CREATE TABLE first_table (id INT NOT NULL AUTO_INCREMENT,
+name VARCHAR(256) NOT NULL,
+PRIMARY KEY (id));
