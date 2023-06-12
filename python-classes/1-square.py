7bd9ce143bbd4ef21bd68651b5caaf92cd3521f0
GuillermoRosario <guillermorosario628@gmail.com>
2023-06-12 13:30:51 -0400

    init class

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Creation of an empty class defining a Square"""
+
+
+class Square:
+    """Write an empty class square that defines a square"""
+    def __init__(self, size):
+        self.__size = size
