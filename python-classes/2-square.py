e6199263ef3396341911132c313327df2c47aa35
GuillermoRosario <guillermorosario628@gmail.com>
2023-06-12 13:59:19 -0400

    PEP8 Correction

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Creation of an empty class defining a Square"""
+
+
 class Square:
     """Write an empty class square that defines a square"""
-    def __init__(self, size):
-        self.__size = size
-        if type (size) != int: 
+    def __init__(self, size=0):
+        if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
