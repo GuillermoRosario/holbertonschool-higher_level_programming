3300b04d72921df839b911c47adc8ccade1be00f
GuillermoRosario <guillermorosario628@gmail.com>
2023-06-14 15:26:54 -0400

    practice exercises

---
+++ python_exercises/Motor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+class Motorcycle:
+    def __init__(self,make,model,year,color):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.color = color
+
+def drive(self):
+    print("vrumm")
+
+def stop(self):
+    print("Stop!")
