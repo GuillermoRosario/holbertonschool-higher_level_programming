7ed24e672338689a4bac3f86e72bc3a83493afa5
GuillermoRosario <guillermorosario628@gmail.com>
2023-06-14 11:18:46 -0400

    Print Square

---
+++ python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""Creation of an empty class defining a Square"""
+
+
+class Square:
+    """Write an empty class square that defines a square"""
+    def __init__(self, size=0):
+        """Function to retrieve"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        self = self.__size * self.__size
+        return self
+    """Create Getter Function"""
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
---
+++ python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Creation of an empty class defining a Square"""
+
+
+class Square:
+    """Write an empty class square that defines a square"""
+    def __init__(self, size=0):
+        """Function instatiation"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    """Create Getter Function"""
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        self = self.__size * self.__size
+        return self
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
