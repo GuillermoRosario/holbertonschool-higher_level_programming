80ef26bc670819117c20146138352fa76d0bcb59
GuillermoRosario <guillermorosario628@gmail.com>
2023-06-20 12:29:28 -0400

    Variable Identifier

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Creation of an empty class defining a Square"""
 
-
 class Square:
     """Write an empty class square that defines a square"""
     def __init__(self, size=0):
